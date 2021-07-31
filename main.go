29efc29754b57dbcec57b47db08026129c89a626
Pholawat Tangsatit <pholawat.tangsatit@hotmail.com>
2021-07-31 20:30:09 +0700

    Define Parameter and Returns

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
-import "github.com/aws/aws-lambda-go/lambda"
+import (
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
 
 func main() {
 	lambda.Start(HandleEvent)
 }
 
-func HandleEvent() {
+func HandleEvent(e events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 
+	return events.APIGatewayProxyResponse{}, nil
 }
