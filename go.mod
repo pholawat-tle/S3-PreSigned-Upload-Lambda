2ac86b71bc61697a8594c6984d78058d8935f386
Pholawat Tangsatit <pholawat.tangsatit@hotmail.com>
2021-07-31 20:38:29 +0700

    Implement

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module S3-PreSigned-Upload-Lambda
 
 go 1.16
 
-require github.com/aws/aws-lambda-go v1.26.0
+require (
+	github.com/aws/aws-lambda-go v1.26.0
+	github.com/aws/aws-sdk-go-v2/config v1.5.0
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.11.1
+)
--- main.go
+++ main.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
 func main() {
@@ -10,6 +17,34 @@ func main() {
 }
 
 func HandleEvent(e events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	// Set up Lambda Function
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("ap-southeast-1"))
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+	// Create S3 Client
+	client := s3.NewPresignClient(s3.NewFromConfig(cfg))
+	bucketName := "your-s3-bucket-name"
+	// Get file name from Request
+	var RequestBody interface{}
+	json.Unmarshal([]byte(e.Body), &RequestBody)
+	key := RequestBody.(map[string]interface{})["Key"].(string)
+	// Generate Presigned URL for upload
+	params := s3.PutObjectInput{
+		Bucket: &bucketName,
+		Key:    &key,
+	}
 
-	return events.APIGatewayProxyResponse{}, nil
+	PresignedRequest, err := client.PresignPutObject(context.TODO(), &params)
+	if err != nil {
+		return events.APIGatewayProxyResponse{}, err
+	}
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusOK,
+		Headers: map[string]string{
+			"Content-Type": "application/json",
+		},
+		Body: fmt.Sprintf(`{"PresignedURL": %s`, PresignedRequest.URL),
+	}, nil
 }
